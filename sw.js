75bec0dc59fc9a6a0b1097c1a8a5d33f4818ce28
marieaudekouame10-spec <marieaudekouame10@gmail.com>
2026-07-19 21:18:32 +0000

    Add Service Worker for offline support and caching

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Service Worker pour support offline et cache
+
+const CACHE_NAME = 'wedding-companion-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js'
+];
+
+// Installation
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        }).catch(() => {
+            return new Response('Offline - contenu non disponible');
+        })
+    );
+});
+
+// Activation
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
